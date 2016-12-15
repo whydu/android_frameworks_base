3878d39966375245850022e1787fcfcb26f3c780
mydongistiny <jaysonedson@gmail.com>
2016-12-15 14:53:38 -0800

    SystemUI: add toggle for the onehand ui mode [1/2]
    with https://github.com/BenzoRoms/frameworks_base/commit/6eb39f2a8248b44894ad2d657cf8ba17144c93f3
    swiping either left or right on the navbar will enable onehand ui on the side you you swipe towards
    not everyone likes having it on so give them an option to turn it off
    
    Change-Id: I581db476bc8a9acc14594a89114b65950cfda402

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4852,6 +4852,15 @@ public final class Settings {
         public static final String THEME_TILE_ENABLED_MODE = "theme_tile_enabled_mode";
 
         /**
+         * enable or disable single handed mode
+         * @hide
+         */
+        public static final String ONE_HAND_MODE_ENABLED = "one_hand_mode_enabled";
+
+        /** @hide */
+        private static final Validator ONE_HAND_MODE_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4920,6 +4929,7 @@ public final class Settings {
             OMNI_USE_BOTTOM_GESTURE_NAVIGATION,
             OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT,
             OMNI_BOTTOM_GESTURE_SWIPE_LIMIT,
+            ONE_HAND_MODE_ENABLED
         };
 
         /**
@@ -5058,6 +5068,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_USE_BOTTOM_GESTURE_NAVIGATION);
             PRIVATE_SETTINGS.add(OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT);
             PRIVATE_SETTINGS.add(OMNI_BOTTOM_GESTURE_SWIPE_LIMIT);
+            PRIVATE_SETTINGS.add(ONE_HAND_MODE_ENABLED);
         }
 
         /**
@@ -5166,6 +5177,7 @@ public final class Settings {
                     OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT_VALIDATOR);
             VALIDATORS.put(OMNI_BOTTOM_GESTURE_SWIPE_LIMIT,
                     OMNI_BOTTOM_GESTURE_SWIPE_LIMIT_VALIDATOR);
+            VALIDATORS.put(ONE_HAND_MODE_ENABLED, ONE_HAND_MODE_ENABLED_VALIDATOR);
         }
 
         /**
